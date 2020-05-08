7e4db563f3abdd04844173c841d336393278b281
Brendon Henning <brendonhenning@gmail.com>
2020-05-07 18:03:44 -0600

    Clear button works.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
-add = (a, b) => a + b;
-subtract = (a,b) => a - b;
-multiply = (a,b) => a * b;
-divide = (a,b) => a / b;
+const add = (a, b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
 
-operate = (a,b,c) => {
+const operate = (a,b,c) => {
     if (b == '+') return add(a, c);
     if (b == '-') return subtract(a, c);
     if (b == '*') return multiply(a, c);
@@ -12,16 +12,20 @@ operate = (a,b,c) => {
 
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
+const clear = document.querySelector('#clear');
 let buttonClicks = []
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
+        if (e.target.value !== 'C') {
     buttonClicks.push(e.target.value)
     let screen = buttonClicks.join('')
+    display.textContent = `${screen}`
     console.log(buttonClicks)
     console.log(screen)
-    display.textContent = `${screen}`
+    } else {
+        buttonClicks.length = 0;
+        display.textContent = '';
+    }
 })
 });
 
-
-//make variable an array, and assign clicks to the indexes of array
