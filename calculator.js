5c760dd0cd342eb320c6a430d32aa14d5f385f9e
Brendon Henning <brendonhenning@gmail.com>
2020-05-12 11:11:06 -0600

    Calc works with multiple operations. Does not work when continuing operations after hitting equals.

--- calculator.js
+++ calculator.js
@@ -16,6 +16,9 @@ let args1 = '';
 let args2 = '';
 let firstOp = '';
 let workingArray = []
+let longArray = []
+let pick = ''
+let targetClass = ''
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
     let pick = e.target.value;
@@ -24,32 +27,55 @@ buttons.forEach(button => {
         return clear()
     } else if (targetClass == 'number button') {
         workingArray.push(pick)
+        longArray.push(pick)
         let screen = workingArray.join('')
         display.textContent = `${screen}`;
-        console.log(workingArray);
-    } else if (targetClass == 'operator button') {
-        args1 = workingArray.slice(0, this.index).join('')
-        firstOp = this.value;
-        display.textContent = pick
-        workingArray = [];
+    } 
+    if (targetClass == 'operator button') {
+        longArray.push(this.value)
+        let opCount = longArray.reduce((mode, val) => {
+            if (val == '+' || val == '-' || val == '/' || val == '*') {
+                mode += 1
+            }
+            return mode;
+        },0);
+        console.log(opCount)
+        if (opCount > 1) {
+            a = Number(args1);
+            b = firstOp;
+            c = Number(workingArray.slice(0, this.index).join(''));
+            args1 = operate(a,b,c);
+            display.textContent = args1;
+            workingArray = []
+            firstOp = this.value //assigns the operator for next operation. 
+            } 
+        if (opCount == 1) {
+            args1 = workingArray.slice(0, this.index).join('')
+            workingArray = [];
+            firstOp = this.value;
+            display.textContent = pick
+            console.log(args1)
+        }
     } else if (pick == '=') {
         if (args1 == '') return;
         a = Number(args1);
         b = firstOp;
         c = Number(workingArray.slice(0, this.index).join(''));
-        console.log(a)
-        console.log(b)
-        console.log(c)
-        let result = operate(a,b,c);
-        display.textContent = result;
-    }
+        args1 = operate(a,b,c);
+        display.textContent = args1;
+        longArray = [];
+    } 
 });
 });
 function clear() {
     workingArray.length = 0;
     display.textContent = '';
+    longArray.length = 0;
 }
 
 
+    
 
-//once operator is pressed, set the array to Args1. if operator is pressed again, args 2 becomes args 1. 
+    
+ 
+
