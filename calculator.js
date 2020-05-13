c2dd1ceba3d981df7c395d497546c93b7f51551a
Brendon Henning <brendonhenning@gmail.com>
2020-05-13 09:31:33 -0600

    fresh operations now work after equals is pressed.

--- calculator.js
+++ calculator.js
@@ -13,23 +13,29 @@ const operate = (a,b,c) => {
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 let args1 = '';
-let args2 = '';
 let firstOp = '';
 let workingArray = []
 let longArray = []
-let pick = ''
 let targetClass = ''
+let eqCheck = '';
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
     let pick = e.target.value;
     let targetClass = e.target.getAttribute('class')
     if (pick == 'C') {
         return clear()
-    } else if (targetClass == 'number button') {
+    }
+    if (targetClass == 'number button') {
         workingArray.push(pick)
         longArray.push(pick)
         let screen = workingArray.join('')
         display.textContent = `${screen}`;
+        console.log(eqCheck)
+        console.log(args1)
+        if (eqCheck == true) {
+            args1 = '';
+            eqCheck = false;
+        }
     } 
     if (targetClass == 'operator button') {
         longArray.push(this.value)
@@ -39,8 +45,8 @@ buttons.forEach(button => {
             }
             return mode;
         },0);
-        console.log(opCount)
-        if (opCount > 1) {
+        eqCheck = false;
+        if (opCount > 1) {   //counts the number of existing operators
             a = Number(args1);
             b = firstOp;
             c = Number(workingArray.slice(0, this.index).join(''));
@@ -49,28 +55,28 @@ buttons.forEach(button => {
             workingArray = []
             firstOp = this.value //assigns the operator for next operation. 
             } 
-        if (opCount == 1 && args1 == []) { //checks if there is an args1 assigned from equals button being hit previously
+        if (opCount == 1 && args1 == []) { //checks to make sure there isn't an args1 assigned from equals button being hit previously
             args1 = workingArray.slice(0, this.index).join('')
             console.log(args1)
             workingArray = [];
             firstOp = this.value;
             display.textContent = pick
-        } else if (opCount == 1) {
-            console.log(args1)
+        } 
+        if (opCount == 1) { //runs if there is already an args1 from previous operation
             workingArray = [];
             firstOp = this.value;
-            display.textContent = pick
+            display.textContent = pick;
         }
     } else if (pick == '=') {
-        if (args1 == '') return;
         a = Number(args1);
         b = firstOp;
         c = Number(workingArray.slice(0, this.index).join(''));
         args1 = operate(a,b,c);
+        eqCheck = true; 
         display.textContent = args1;
         longArray = []
-        console.log(args1)
-        console.log(workingArray)
+        workingArray = [];
+        console.log(eqCheck)
     } 
 });
 });
@@ -78,11 +84,11 @@ function clear() {
     workingArray.length = 0;
     display.textContent = '';
     longArray.length = 0;
+    args1 = ''
 }
-console.log(longArray)
+
 
     
 
-    
- 
-
+    //use a temporary variable to store arg1 after = ?
+    //= assings a variable test. if number is pressed and test is true, set args1 to '' and turn test off. 
