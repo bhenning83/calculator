f69f0e0d30edfae0f96c350871913669e6a89a95
Brendon Henning <brendonhenning@gmail.com>
2020-05-13 12:58:29 -0600

    reformatted/cleaned js doc

--- calculator.js
+++ calculator.js
@@ -1,28 +1,9 @@
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-function divide(a,b) {
-    if (b == 0) {
-        return 'Nice try, smartass'
-    } else {
-        return a / b;
-    }
-}
-
-const operate = (a,b,c) => {
-    if (b == '+') return add(a, c);
-    if (b == '-') return subtract(a, c);
-    if (b == '*') return multiply(a, c);
-    if (b == '/') return divide(a, c);
-}
-
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 let args1 = '';
 let firstOp = '';
 let workingArray = []
 let longArray = []
-let targetClass = ''
 let eqCheck = '';
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
@@ -36,8 +17,6 @@ buttons.forEach(button => {
         longArray.push(pick)
         let screen = workingArray.join('')
         display.textContent = `${screen}`;
-        console.log(eqCheck)
-        console.log(args1)
         if (eqCheck == true) { // erases args1 from previous operation if a fresh operation is being started
             args1 = '';
             eqCheck = false;
@@ -52,18 +31,8 @@ buttons.forEach(button => {
             }
             return mode;
         },0);
-        if (opCount > 1) {   // counts the number of existing operators
-            a = Number(args1);
-            b = firstOp;
-            c = Number(workingArray.slice(0, this.index).join(''));
-            args1 = operate(a,b,c);
-            display.textContent = args1;
-            workingArray = []
-            firstOp = this.value // assigns the operator for next operation. 
-            } 
         if (opCount == 1 && args1 == []) { // checks to make sure there isn't an args1 assigned from equals button being hit previously
             args1 = workingArray.slice(0, this.index).join('')
-            console.log(args1)
             workingArray = [];
             firstOp = this.value;
             display.textContent = pick
@@ -73,6 +42,15 @@ buttons.forEach(button => {
             firstOp = this.value;
             display.textContent = pick;
         }
+        if (opCount > 1) {   // counts the number of existing operators
+            a = Number(args1);
+            b = firstOp;
+            c = Number(workingArray.slice(0, this.index).join(''));
+            args1 = operate(a,b,c);
+            display.textContent = args1;
+            workingArray = []
+            firstOp = this.value // assigns the operator for next operation. 
+            } 
     } else if (pick == '=') {
         a = Number(args1);
         b = firstOp;
@@ -82,7 +60,6 @@ buttons.forEach(button => {
         display.textContent = args1;
         longArray = []
         workingArray = [];
-        console.log(eqCheck)
     } 
 });
 });
@@ -92,7 +69,22 @@ function clear() {
     longArray.length = 0;
     args1 = ''
 }
-
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+function divide(a,b) {
+    if (b == 0) {
+        return 'Nice try, smartass'
+    } else {
+        return a / b;
+    }
+}
+const operate = (a,b,c) => {
+    if (b == '+') return add(a, c);
+    if (b == '-') return subtract(a, c);
+    if (b == '*') return multiply(a, c);
+    if (b == '/') return divide(a, c);
+}
 
     
 
